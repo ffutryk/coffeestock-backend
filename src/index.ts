7a4870669f07bc5c2583ad506f3d07098bb7526c
ffutryk <ffutryk52@gmail.com>
2026-05-03 20:50:08 +0000

    feat: inicializar servidor express básico

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+import express, { type Request, type Response } from "express";
+import { PORT } from "./config/envs.js";
+import { AppDataSource } from "./config/data-source.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+try {
+  await AppDataSource.initialize();
+  app.listen(PORT, () => console.log(`Ready, ${PORT}`));
+} catch (err) {
+  console.error("Error al conectar la base de datos:", err);
+  process.exit(1);
+}
