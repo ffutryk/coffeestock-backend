f5ecad3071f00aa72fbe28677b21585ef8502036
ffutryk <ffutryk52@gmail.com>
2026-05-07 07:06:56 +0000

    feat: registrar middleware de manejo de errores en la aplicación

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import { DEBUG, PORT } from "./config/envs.js";
 import { AppDataSource } from "./config/data-source.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(cors());
 app.get("/", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
+
+app.use(errorHandler);
 
 try {
   await AppDataSource.initialize();
