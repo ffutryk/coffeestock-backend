54353f63424b946d3e38360fe16b3b13e73e2ce0
ManuMenager <emanuelmenager2003@gmail.com>
2026-05-09 13:02:14 -0300

    Agrega DTO de venta e itemVenta

---
+++ src/dtos/venta.dto.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+import { TipoItemVenta } from "../models/TipoItemVenta.js";
+
+export const CrearItemVentaSchema = z.object({
+  nombre: z.string().min(1, "El nombre del item de venta es requerido"),
+  cantidad: z.number().int().positive("La cantidad debe ser mayor a 0"),
+  precio: z.number().positive("El precio debe ser mayor a 0"),
+  tipo: z.enum(TipoItemVenta)
+});
+
+export const CrearVentaSchema = z.object({
+  medioDePago: z.string().min(1, "El medio de pago es requerido"),
+  createdBy: z.number().int(),
+  items: z.array(CrearItemVentaSchema).min(1, "Debe haber al menos un item en la venta"),
+});
+
+export type CrearVentaDTO = z.infer<typeof CrearVentaSchema>;
+export type CrearItemVentaDTO = z.infer<typeof CrearItemVentaSchema>;
