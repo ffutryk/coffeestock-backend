b397a72335de06b04d793920acef961b46657343
ffutryk <ffutryk52@gmail.com>
2026-05-10 01:52:43 +0000

    feat: agregar middleware para la validación de queries en una request

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import type { Request, Response, NextFunction } from "express";
 
 type BodySchema = z.ZodTypeAny;
+type QuerySchema = z.ZodTypeAny;
 
 export function validateBody(schema: BodySchema) {
   return async (req: Request, _res: Response, next: NextFunction) => {
@@ -12,6 +13,20 @@ export function validateBody(schema: BodySchema) {
     }
 
     req.body = result.data;
+
+    next();
+  };
+}
+
+export function validateQuery(schema: QuerySchema) {
+  return async (req: Request, _res: Response, next: NextFunction) => {
+    const result = await schema.safeParseAsync(req.query);
+
+    if (!result.success) {
+      return next(result.error);
+    }
+
+    req.validatedQuery = result.data;
 
     next();
   };
