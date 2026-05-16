e828c1f859dfe84575f067112b160536835c7834
ManuMenager <emanuelmenager2003@gmail.com>
2026-05-16 20:15:21 -0300

    Agrega listado de usuarios paginado

--- src/services/usuario.service.ts
+++ src/services/usuario.service.ts
@@ -1,17 +1,24 @@
 import type { IUsuarioRepository } from "../repositories/interfaces/usuario.interface";
 import type { Usuario } from "../models/entities/usuario";
+import type { PaginacionDTO } from "../dtos/paginacion.dto";
+import type { ResultadoPaginado } from "../models/types/resultado-paginado";
 
 export type UsuarioSinPassword = Omit<Usuario, "password">;
 
 export class UsuarioService {
   constructor(private readonly usuarioRepo: IUsuarioRepository) {}
 
-  async listarUsuarios(): Promise<UsuarioSinPassword[]> {
-    const usuarios = await this.usuarioRepo.findAll();
+  async listarUsuarios(paginacion: PaginacionDTO): Promise<ResultadoPaginado<UsuarioSinPassword>> {
+    const resultado = await this.usuarioRepo.findAllPaginated(paginacion);
     
-    return usuarios.map((usuario) => {
+    const dataSinPassword = resultado.data.map((usuario) => {
       const { password, ...usuarioSinPassword } = usuario;
       return usuarioSinPassword as UsuarioSinPassword;
     });
+
+    return {
+      data: dataSinPassword,
+      total: resultado.total,
+    };
   }
 }
