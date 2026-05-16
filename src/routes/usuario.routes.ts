a72d611bf78df001f2b16a1d315108a213f20e8d
ManuMenager <emanuelmenager2003@gmail.com>
2026-05-16 20:15:02 -0300

    Modifica ruta provisoria sin auth

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -2,15 +2,14 @@ import { Router } from "express";
 import { UsuarioService } from "../services/usuario.service";
 import { UsuarioController } from "../controllers/usuario.controller";
 import { TypeOrmUsuarioRepository } from "../repositories/typeorm/usuario.repository";
-import { verificarRol } from "../middlewares/auth";
-import { RolUsuario } from "../models/enums/rol-usuario";
 
 const router = Router();
 const usuarioDao = new TypeOrmUsuarioRepository();
 const usuarioService = new UsuarioService(usuarioDao);
 const usuarioController = new UsuarioController(usuarioService);
 
-// Ruta para ver listado de usuarios, restringida a GERENTE
-router.get("/", verificarRol(RolUsuario.GERENTE), usuarioController.listar);
+// Descomentar la siguiente línea y eliminar la pública cuando se implemente la autenticación
+// router.get("/", verificarRol(RolUsuario.GERENTE), usuarioController.listar);
+router.get("/", usuarioController.listar);
 
 export default router;
