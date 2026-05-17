9e7d9c55cacde9ff016600e9de22840643ffc8ce
Franco Rodriguez <francototites@gmail.com>
2026-05-17 20:44:36 -0300

    feat: agrego DTOresponse para los usuarios

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,6 +1,7 @@
 import type {NextFunction, Request, Response,} from "express";
 import { UsuarioService } from "../services/usuario.service";
 import { BadRequestError } from "../errors";
+import { UsuarioResponseDTO } from "../dtos/usuario/response.dto";
 
 export default class UsuarioController {
 
@@ -9,7 +10,8 @@ export default class UsuarioController {
     crear = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const creado = await this.usuarioService.crearUsuario(req.body);
-            return res.status(201).json({success: true, message: "Empleado registrado exitosamente", data: creado});
+            const respuesta = new UsuarioResponseDTO(creado);
+            return res.status(201).json({success: true, message: "Empleado registrado exitosamente", data: respuesta});
         } catch (err) {
             next(err);
         }
@@ -26,7 +28,8 @@ export default class UsuarioController {
                 throw new BadRequestError("ID inválido");
             }
             const actualizado = await this.usuarioService.actualizarUsuario(id, req.body);
-            return res.status(200).json({success: true, data: actualizado});
+            const respuesta = new UsuarioResponseDTO(actualizado);
+            return res.status(200).json({success: true, data: respuesta});
         } catch (err) {
             next(err);
         }
