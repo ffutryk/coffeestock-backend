0af0ab108268aaecf9cf19c19741bb74ccac7699
ManuMenager <emanuelmenager2003@gmail.com>
2026-05-25 16:40:36 -0300

    Agrega ruta para las estadisticas de las ventas, con permiso solo para Gerentes

--- src/routes/estadistica.routes.ts
+++ src/routes/estadistica.routes.ts
@@ -3,7 +3,10 @@ import { EstadisticaController } from "../controllers/estadistica.controller";
 import { EstadisticaService } from "../services/estadistica.service";
 import { TypeORMEstadisticaRepository } from "../repositories/typeorm/estadistica.repository";
 import { esGerente } from "../middlewares/role.middleware";
-// despues se deberia importar la verificacion de autenticidad que se usa en el login
+import { verificarRol } from "../middlewares/auth";
+import { RolUsuario } from "../models/enums/rol-usuario";
+import { validateQuery } from "../middlewares/validate";
+import { FiltrarEstadisticasVentasSchema } from "../dtos/estadistica/filtrar-ventas.dto";
 
 const router = Router();
 
@@ -11,11 +14,7 @@ const estadisticaRepository = new TypeORMEstadisticaRepository();
 const estadisticaService = new EstadisticaService(estadisticaRepository);
 const estadisticaController = new EstadisticaController(estadisticaService);
 
-router.get(
-  "/productos",
-  // verificarAuth,
-  esGerente,
-  estadisticaController.obtenerEstadisticasProductos
-);
+router.get("/ventas", verificarRol([RolUsuario.GERENTE]), validateQuery(FiltrarEstadisticasVentasSchema), estadisticaController.obtenerEstadisticasVentas);
+router.get("/productos", verificarRol([RolUsuario.GERENTE]), estadisticaController.obtenerEstadisticasProductos);
 
 export default router;
