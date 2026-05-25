41e0fdde241551e4fd9e37a12f480f0f0d6bc102
ManuMenager <emanuelmenager2003@gmail.com>
2026-05-25 16:40:05 -0300

    Agrega método obtenerEstadisticasVentas en el service.

--- src/services/estadistica.service.ts
+++ src/services/estadistica.service.ts
@@ -1,9 +1,27 @@
-import { EstadisticaDao } from "../repositories/interfaces/estadistica.interface";
+import { EstadisticaDao, EstadisticasVentasDTO } from "../repositories/interfaces/estadistica.interface";
 import { ReporteEstadisticasDTO } from "../models/types/estadisticas";
 
 export class EstadisticaService {
 
   constructor(private estadisticaDao: EstadisticaDao) {}
+
+  async obtenerEstadisticasVentas(fechaInicio?: string, fechaFin?: string): Promise<EstadisticasVentasDTO> {
+    let inicio: Date | undefined;
+    let fin: Date | undefined;
+
+    if (fechaInicio) {
+      inicio = new Date(fechaInicio);
+    }
+
+    if (fechaFin) {
+      fin = new Date(fechaFin);
+      if (fechaFin.indexOf("T") === -1) {
+        fin.setHours(23, 59, 59, 999);
+      }
+    }
+
+    return await this.estadisticaDao.obtenerEstadisticasVentas(inicio, fin);
+  }
 
   async obtenerEstadisticasProductos(user: string): Promise<ReporteEstadisticasDTO> {
     if(!user) {
