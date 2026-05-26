4d161421498b8515545e682538593dd24daeebcc
Uriel Benitez <unbenitez@gmail.com>
2026-05-25 21:45:18 -0300

    feat(cliente): instancia axios con interceptor y servicio auth

---
+++ client/src/services/auth.js
@@ -0,0 +1,11 @@
+import api from "./api";
+
+export async function login(credentials) {
+  const res = await api.post("/usuarios/ingresar", credentials);
+  return res.data; // token string
+}
+
+export async function registerUsuario(data) {
+  const res = await api.post("/usuarios/crear", data);
+  return res.data;
+}
