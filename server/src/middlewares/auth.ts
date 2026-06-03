eb704f46438fad530a7f759cc6f6bcb019690dc2
ffutryk <ffutryk52@gmail.com>
2026-06-03 02:22:51 +0000

    refactor: utilizar usuarioServices del DI

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -2,13 +2,10 @@ import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/envs";
 import { UnauthorizedError } from "../errors";
-import { TypeOrmUsuarioRepository } from "../repositories/typeorm/usuario.repository";
 import { TokenPayload } from "../models/types/token-payload";
-import { UsuarioService } from "../services/usuario.service";
 import { AuthContext } from "../context/auth.context";
+import { usuarioService } from "../container/di";
 
-const usuarioRepository = new TypeOrmUsuarioRepository();
-const usuarioService = new UsuarioService(usuarioRepository);
 // A futuro deberiamos crear containers para no repetir la creacion e inyección de servicios, así de paso se utiliza la misma instancia y configuración
 
 export const auth = async (req: Request, _res: Response, next: NextFunction) => {
