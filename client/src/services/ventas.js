9255a35c29525e64fe61915bbf988ed94ac1f37f
Uriel Benitez <unbenitez@gmail.com>
2026-06-08 01:13:33 -0300

    feat: se implementa funcionalidad para eliminar ventas

--- client/src/services/ventas.js
+++ client/src/services/ventas.js
@@ -9,3 +9,8 @@ export const actualizarVenta = async (id, data) => {
   const response = await api.put(`/ventas/${id}`, data);
   return response.data;
 };
+
+export const eliminarVenta = async (id) => {
+  const response = await api.delete(`/ventas/${id}`);
+  return response.data;
+};
